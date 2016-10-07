cf659f9de98cd398816dac56a54fbdae36ea3dc6
Jaesin Mulenex <jaesin@no-reply.gradientsky.com>
2016-10-07 16:27:44 -0700

    [Testing] Adds factory method tests.

--- src/TeamworkClient.php
+++ src/TeamworkClient.php
@@ -12,6 +12,18 @@ class TeamworkClient extends GuzzleClient implements TeamworkClientInterface {
      * {@inheritdoc}
      */
     public static function create($config = []) {
+
+        // Required config.
+        $required = [
+            'base_uri',
+            'api_token',
+        ];
+
+        // Enforce required configuration.
+        if ($missing = array_diff($required, array_keys($config))) {
+            throw new \InvalidArgumentException('Config is missing the following keys: ' . implode(', ', $missing));
+        }
+
         // Load the service description file.
         $service_description = new Description(
             ['baseUrl' => $config['base_uri']] + (array) json_decode(file_get_contents(__DIR__ . '/service.json'), TRUE)
