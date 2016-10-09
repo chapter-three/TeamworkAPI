579ecf5a0555b7d40f8abae8f7e7df95bd3d28e8
Jaesin Mulenex <jaesin@no-reply.gradientsky.com>
2016-10-08 19:35:22 -0700

    [Testing] Add a command test that uses the built in guzzle test server.

--- tests/TeamworkApiClient/TeamworkApiClientTest.php
+++ tests/TeamworkApiClient/TeamworkApiClientTest.php
@@ -4,6 +4,9 @@ namespace ChapterThree\TeamworkAPI\Tests\TeamworkApiClient;
 
 use ChapterThree\TeamworkAPI\TeamworkClient;
 use ChapterThree\TeamworkAPI\TeamworkClientInterface;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Tests\Server;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -11,21 +14,6 @@ use PHPUnit_Framework_TestCase;
  */
 
 class TeamworkApiClientTest extends PHPUnit_Framework_TestCase {
-
-    /**
-     * @var \ChapterThree\TeamworkAPI\TeamworkClientInterface
-     */
-    protected $client;
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->client = TeamworkClient::create([
-            'base_uri' => $_SERVER['BASE_URI'],
-            'api_token' => $_SERVER['API_TOKEN'],
-        ]);
-    }
 
     public function testFactoryMethod()
     {
@@ -48,6 +36,53 @@ class TeamworkApiClientTest extends PHPUnit_Framework_TestCase {
         ]);
 
         self::assertInstanceOf(TeamworkClientInterface::class, $client);
+    }
+
+    /**
+     * Tests the account without actually making an external api call.
+     */
+    public function testAccountCommand()
+    {
+
+        $response_data = [
+            'STATUS' => 'OK',
+            'account' => [
+                'companyname' => 'Foo',
+                'id'          => '12345',
+                'code'        => 'foo',
+            ],
+        ];
+
+        Server::enqueue([$this->getJsonResponse(200, $response_data)]);
+
+        $client = TeamworkClient::create([
+            'base_uri' => Server::$url,
+            'api_token' => $_SERVER['API_TOKEN'],
+        ]);
+
+        $result = $client->Account()->toArray();
+
+        self::assertArraySubset([
+            'STATUS' => 'OK'
+        ], $result);
+
+        self::assertNotEmpty($result['account']['companyname']);
+    }
+
+    /**
+     * Converts an array to a stream resource.
+     *
+     * @param array $resource_data
+     * @return \GuzzleHttp\Psr7\Response
+     */
+    protected function getJsonResponse($status_code, $resource_data) {
+        $encoded = json_encode($resource_data, JSON_UNESCAPED_SLASHES);
+        // Convert the JSON to a resource.
+        $handle = fopen('php://temp','r+');
+        fwrite($handle, $encoded);
+        rewind($handle);
+
+        return new Response($status_code, ['Content-Length' => strlen($encoded)], new Stream($handle));
     }
 
 }
