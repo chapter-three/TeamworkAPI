e7f114e0d794dadfc4a0c65239a3a6c2c6653f8b
Jaesin Mulenex <jaesin@no-reply.gradientsky.com>
2016-10-13 15:58:28 -0700

    [Refactor] Clean up the tests and add test for authenticaiton header.

--- tests/TeamworkApiClient/TeamworkApiClientTest.php
+++ tests/TeamworkApiClient/TeamworkApiClientTest.php
@@ -15,6 +15,25 @@ use PHPUnit_Framework_TestCase;
 
 class TeamworkApiClientTest extends PHPUnit_Framework_TestCase {
 
+    /**
+     * @var \ChapterThree\TeamworkAPI\TeamworkClient
+     */
+    public $client;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Start the guzzle test server.
+        Server::start();
+        register_shutdown_function(function(){Server::stop();});
+
+        $this->client = TeamworkClient::create([
+            'base_uri' => Server::$url,
+            'api_token' => $_SERVER['API_TOKEN'],
+        ]);
+    }
+
     public function testFactoryMethod()
     {
         $client = TeamworkClient::create([
@@ -36,6 +55,20 @@ class TeamworkApiClientTest extends PHPUnit_Framework_TestCase {
         ]);
 
         self::assertInstanceOf(TeamworkClientInterface::class, $client);
+    }
+
+    /**
+     * Tests the account without actually making an external api call.
+     */
+    public function testAuthentication()
+    {
+        Server::enqueue([$this->getJsonResponse(200, '{"STATUS":"OK","account":{"companyname":"Foo","id":"12345","code":"foo"}}')]);
+
+        $this->client->Account();
+        $request = Server::received()[0];
+
+        self::assertNotEmpty($request->getHeader('Authorization'));
+        self::assertEquals('Basic '. base64_encode("{$_SERVER['API_TOKEN']}:xxx"), $request->getHeader('Authorization')[0]);
     }
 
     /**
@@ -72,17 +105,13 @@ class TeamworkApiClientTest extends PHPUnit_Framework_TestCase {
     /**
      * Converts an array to a stream resource.
      *
-     * @param array $resource_data
+     * @param array|string $resource_data
      * @return \GuzzleHttp\Psr7\Response
      */
     protected function getJsonResponse($status_code, $resource_data) {
-        $encoded = json_encode($resource_data, JSON_UNESCAPED_SLASHES);
-        // Convert the JSON to a resource.
-        $handle = fopen('php://temp','r+');
-        fwrite($handle, $encoded);
-        rewind($handle);
+        $encoded = is_string($resource_data) ? $resource_data : json_encode($resource_data, JSON_UNESCAPED_SLASHES);
 
-        return new Response($status_code, ['Content-Length' => strlen($encoded)], new Stream($handle));
+        return new Response($status_code, ['Content-Length' => strlen($encoded)], $encoded);
     }
 
 }
